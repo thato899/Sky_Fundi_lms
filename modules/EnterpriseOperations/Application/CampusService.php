4ec72096083364c8c06510967aafdea2a09eb816
THATO MALULEKA <thatom505@gmail.com>
2026-08-09 20:50:41 +0200

    Correct Enterprise Operations CI formatting

--- modules/EnterpriseOperations/Application/CampusService.php
+++ modules/EnterpriseOperations/Application/CampusService.php
@@ -15,7 +15,6 @@ final class CampusService
     public const FLAG = 'enterprise.multi_campus';
 
     public function __construct(private readonly FeatureFlagService $flags) {}
-    }
 
     public function enabled(Organization $organization): bool
     {
